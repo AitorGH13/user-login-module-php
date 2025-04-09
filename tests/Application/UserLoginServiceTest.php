6cb70c67a5a78ce0635017817c46ec51461d5339
Aitor <arrarasaitor@gmail.com>
2025-04-09 11:17:01 +0200

    [*] - Test user already logged in added

--- tests/Application/UserLoginServiceTest.php
+++ tests/Application/UserLoginServiceTest.php
@@ -6,16 +6,34 @@ namespace UserLoginService\Tests\Application;
 
 use PHPUnit\Framework\TestCase;
 use UserLoginService\Application\UserLoginService;
+use UserLoginService\Domain\User;
 
 final class UserLoginServiceTest extends TestCase
 {
     /**
      * @test
      */
-    public function userIsLoggedIn()
+    public function userIsLoggedIn(): void
     {
+        $user = new User("Iñigo");
         $userLoginService = new UserLoginService();
 
-        $this->assertEquals("user logged", $userLoginService->manualLogin());
+        $userLoginService->manualLogin($user);
+
+        $this->assertEquals("Iñigo", $userLoginService->getLoggedUser($user));
+    }
+
+    /**
+     * @test
+     */
+    public function userAlreadyLoggedIn(): void
+    {
+        $user = new User("Iñigo");
+        $userLoginService = new UserLoginService();
+
+        $this->expectExceptionMessage("User already logged in");
+
+        $userLoginService->manualLogin($user);
+        $userLoginService->manualLogin($user);
     }
 }
